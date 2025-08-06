99b8a9be9844147360df07b8e74c0dc5dc139c34
HarrisonKanellos <harrisonkanellos@gmail.com>
2025-08-07 01:00:18 +1000

    Define initEventListeners function, include addSpace click listener

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import "./styles/style.css";
 import * as Logic from "./logic.js";
 import * as Display from "./display.js";
+
+function initEventListeners() {
+    const addSpaceButton = document.querySelector("#add-space-button");
+    
+    addSpaceButton.addEventListener("click", handleAddSpace);
+}
