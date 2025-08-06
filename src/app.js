db9ee881145147ae049d831f8bea218f4776b635
HarrisonKanellos <harrisonkanellos@gmail.com>
2025-08-07 01:43:31 +1000

    Create submitAddSpace click event and handlers for add space events
    Create handler for handleOpenAddSpaceModal and handleSubmitAddSpace.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,29 @@ import * as Display from "./display.js";
 
 function initEventListeners() {
     const addSpaceButton = document.querySelector("#add-space-button");
-    
-    addSpaceButton.addEventListener("click", handleAddSpace);
+    const submitAddSpaceButton = document.querySelector("#add-space");
+
+    addSpaceButton.addEventListener("click", handleOpenAddSpaceModal);
+    submitAddSpaceButton.addEventListener("click", handleSubmitAddSpace);
+}
+
+function handleOpenAddSpaceModal() {
+    const addSpaceModal = document.querySelector(".modal-add-space");
+    Display.displayModal(addSpaceModal);
+}
+
+function handleSubmitAddSpace() {
+    const addSpaceModal = document.querySelector(".modal-add-space");
+    const spaceNameInput = document.querySelector("#space-name");
+
+    spaceName = spaceNameInput.value;
+    if (!addNewSpace(spaceName)) {
+        // Runs if name already exists
+        // Provide UI message to user
+        return;
+    }
+
+    Display.closeModal(addSpaceModal);
+    Display.clearUserMadeSpaces();
+    Display.renderSpaceTabs(Logic.getUserMadeSpaceNames());
 }
