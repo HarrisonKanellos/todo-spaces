cb669c9e168310dc5cfe8e0df1244072b7762640
HarrisonKanellos <harrisonkanellos@gmail.com>
2025-08-18 23:06:21 +1000

    Move event for add todo button to space container with delegation

--- src/app.js
+++ src/app.js
@@ -5,14 +5,12 @@ import * as Display from "./display.js";
 function initEventListeners() {
     const addSpaceButton = document.querySelector("#add-space-button");
     const submitAddSpaceButton = document.querySelector("#add-space");
-    const addTodoButton = document.querySelector("#add-todo-icon");
     const submitAddTodoButton = document.querySelector("#add-todo");
     const spaceContainer = document.querySelector(".space-container");
     const todoItemModal = document.querySelector("#modal-todo-item");
 
     addSpaceButton.addEventListener("click", handleOpenAddSpaceModal);
     submitAddSpaceButton.addEventListener("click", handleSubmitAddSpace);
-    addTodoButton.addEventListener("click", handleOpenAddTodoModal);
     submitAddTodoButton.addEventListener("click", handleSubmitAddTodo);
     spaceContainer.addEventListener("click", handleSpaceContainerClick);
     todoItemModal.addEventListener("click", handleTodoItemModalClick);
@@ -41,32 +39,6 @@ function handleSubmitAddSpace(event) {
     Display.renderSpaceTabs(Logic.getUserMadeSpaceNames());
 }
 
-function handleOpenAddTodoModal() {
-    const addTodoModal = document.querySelector(".modal-add-todo");
-    Display.displayModal(addTodoModal);
-}
-
-function handleSubmitAddTodo(event) {
-    event.preventDefault();
-
-    const addTodoModal = document.querySelector(".modal-add-todo");
-    const title = document.querySelector("#title-input").value;
-    const description = document.querySelector("#description-input").value;
-    const dueDate = document.querySelector("#due-date-input").value;
-    const priority = document.querySelector("#priority-input").value;
-    const status = document.querySelector("#status-input").value;
-    
-    const spaceName = document.querySelector(".space-heading").textContent;
-    const todoDataObj = { title, description, dueDate, priority, status };
-    Logic.addNewTodo(spaceName, todoDataObj);
-
-    Display.closeModal(addTodoModal);
-
-    const todoListArr = Logic.getTodoList(spaceName);
-    Display.clearSpaceList();
-    Display.renderSpaceList(todoListArr);
-    Display.renderTodoCount(todoListArr.length);
-}
 
 function handleSpaceContainerClick(event) {
     const statusCheckbox = event.target.closest(".list-item-checkbox");
@@ -74,16 +46,22 @@ function handleSpaceContainerClick(event) {
         handleStatusToggle(event);
         return;
     }
-
+    
     const deleteButton = event.target.closest(".list-item-delete");
     if (deleteButton) {
         handleDeleteFromSpaceList(event);
         return;
     }
-
+    
     const todoItem = event.target.closest(".space-list-todo");
     if (todoItem) {
         handleTodoItemClick(todoItem);
+        return;
+    }
+    
+    const addTodoButton = event.target.closest("#add-new-todo");
+    if (addTodoButton) {
+        handleOpenAddTodoModal();
     }
 }
 
@@ -91,7 +69,7 @@ function handleStatusToggle(event) {
     const todoContainer = event.target.closest(".space-list-todo");
     const todoID = todoContainer.dataset.id;
     const spaceName = document.querySelector(".space-heading").textContent;
-
+    
     Logic.toggleTodoStatus(spaceName, todoID);
     Display.removeTodoFromSpaceList(todoContainer);
 }
@@ -109,7 +87,7 @@ function handleTodoItemClick(todoItem) {
     const todoID = todoItem.dataset.id;
     const todoObj = Logic.getTodoObjFromID(spaceName, todoID);
     Display.populateTodoItemModal(todoID, todoObj);
-
+    
     const todoItemModal = document.querySelector("#modal-todo-item");
     Display.displayModal(todoItemModal);
 }
@@ -143,7 +121,7 @@ function handleDeleteFromTodoModal() {
     const todoDeleteModal = document.querySelector("#modal-delete-todo");
     const todoID = document.querySelector("#modal-todo-item").dataset.id;
     todoDeleteModal.dataset.id = todoID;
-
+    
     Display.displayModal(todoDeleteModal);
 }
 
@@ -154,7 +132,7 @@ function handleCloseTodoItemModal() {
 
 function handleTodoSaveChanges(event) {
     event.preventDefault();
-
+    
     const todoItemModal = document.querySelector("#modal-todo-item");
     const title = document.querySelector("#todo-title").value;
     const description = document.querySelector("#todo-description").value;
@@ -165,11 +143,38 @@ function handleTodoSaveChanges(event) {
     const spaceName = document.querySelector(".space-heading").textContent;
     const todoID = todoItemModal.dataset.id;
     const todoDataObj = { title, description, dueDate, priority, status };
-
+    
     Logic.updateTodo(spaceName, todoID, todoDataObj);
     Display.closeModal(todoItemModal);
-
+    
     const todoListArr = Logic.getTodoList(spaceName);
     Display.clearSpaceList();
     Display.renderSpaceList(todoListArr);
+}
+
+function handleOpenAddTodoModal() {
+    const addTodoModal = document.querySelector(".modal-add-todo");
+    Display.displayModal(addTodoModal);
+}
+
+function handleSubmitAddTodo(event) {
+    event.preventDefault();
+    
+    const addTodoModal = document.querySelector(".modal-add-todo");
+    const title = document.querySelector("#title-input").value;
+    const description = document.querySelector("#description-input").value;
+    const dueDate = document.querySelector("#due-date-input").value;
+    const priority = document.querySelector("#priority-input").value;
+    const status = document.querySelector("#status-input").value;
+    
+    const spaceName = document.querySelector(".space-heading").textContent;
+    const todoDataObj = { title, description, dueDate, priority, status };
+    Logic.addNewTodo(spaceName, todoDataObj);
+    
+    Display.closeModal(addTodoModal);
+    
+    const todoListArr = Logic.getTodoList(spaceName);
+    Display.clearSpaceList();
+    Display.renderSpaceList(todoListArr);
+    Display.renderTodoCount(todoListArr.length);
 }
